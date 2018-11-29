c279fc76940f99a7972e0d097f11e5bf176849c2
oberstrike <markus.juergens@gmx.de>
2018-11-29 13:41:21 +0100

    Sorting Code

--- SQLiteApplication/Web/PathCreator.cs
+++ SQLiteApplication/Web/PathCreator.cs
@@ -11,54 +11,56 @@ namespace SQLiteApplication.Web
         public string ServerId { get; set; }
         public string VillageId { get; set; }
 
-        private string _basePath;
+        private string GetBasePath(){
+           return $"https://de{ServerId}.die-staemme.de/game.php?";
+        }
+        
 
         public PathCreator(string pServerId, string pVillageId)
         {
             ServerId = pServerId;
             VillageId = pVillageId;
-            _basePath = $"https://de{ServerId}.die-staemme.de/game.php?";
 
         }
 
         public string GetMain()
         {
-            return $"{_basePath}village={ServerId}&screen=main";
+            return $"{GetBasePath()}village={ServerId}&screen=main";
         }
 
         public string GetPlace()
         {
-            return $"{_basePath}village={VillageId}&screen=place&mode=command";
+            return $"{GetBasePath()}village={VillageId}&screen=place&mode=command";
         }
 
         public string GetLogout(string csrf)
         {
-            return $"{_basePath}village={VillageId}&screen=&action=logout&h={csrf}";
+            return $"{GetBasePath()}village={VillageId}&screen=&action=logout&h={csrf}";
         }
 
         public string GetAttackLink(string target)
         {
-            return $"{_basePath}village={VillageId}&screen=place&target={target}";
+            return $"{GetBasePath()}village={VillageId}&screen=place&target={target}";
         }
 
         public string GetFarmAssist()
         {
-            return $"https://de{ServerId}.die-staemme.de/game.php?village={VillageId}&screen=am_farm";
+            return $"{GetBasePath()}&screen=am_farm";
         }
 
         public string GetBuildingOverview()
         {
-            return $"https://de{ServerId}.die-staemme.de/game.php?village={VillageId}&screen=overview_villages";
+            return $"{GetBasePath()}&screen=overview_villages";
         }
 
         public string GetMarketModeSend()
         {
-            return $"https://de{ServerId}.die-staemme.de/game.php?village={VillageId}&screen=market&mode=send";
+            return $"{GetBasePath()}&screen=market&mode=send";
         }
 
         public string GetBarracks()
         {
-            return $"https://de{ServerId}.die-staemme.de/game.php?village={VillageId}&screen=barracks";
+            return $"{GetBasePath()}&screen=barracks";
         }
 
         internal string GetStable()
