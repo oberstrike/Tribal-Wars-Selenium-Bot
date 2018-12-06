bc4031cd597ee11ba8b05cdde1085fe417e2fd50
oberstrike <markus.juergens@gmx.de>
2018-12-06 10:31:20 +0100

    Update Unit.cs

--- SQLiteApplication/VillageData/Unit.cs
+++ SQLiteApplication/VillageData/Unit.cs
@@ -7,8 +7,15 @@ using System.Threading.Tasks;
 namespace SQLiteApplication.VillageData
 {
     public enum Unit {
-        [Unit("spears", 50, 20, 20, 1)] SPEARS
-
+        [Unit("spears", 50, 30, 20, 1)] SPEARS,
+        [Unit("sword", 30, 30, 70, 1)] SWORD,
+        [UNIT("axe", 60, 30, 40, 1)] AXE,
+        [UNIT("spy", 50, 50, 20, 2)] SPY,
+        [UNIT("light", 125, 100, 250, 4)] LIGHT,
+        [UNIT("heavy", 200, 150, 600, 6)] HEAVY,
+        [UNIT("ram", 300, 200, 200, 5) RAM,
+        [UNIT("kata", 320, 400, 10, 8) KATA
+    
     }
 
     public static class UnitExtension
@@ -16,6 +23,10 @@ namespace SQLiteApplication.VillageData
         public static UnitAttribute GetAttribute(this Unit unit)
         {
             return (UnitAttribute)Attribute.GetCustomAttribute(typeof(Unit).GetField(Enum.GetName(typeof(Unit), unit)), typeof(UnitAttribute));
+        }
+        
+        public static int GetWood(this Unit unit){
+            return unit.GetAttribute().Wood;
         }
     }         
 
