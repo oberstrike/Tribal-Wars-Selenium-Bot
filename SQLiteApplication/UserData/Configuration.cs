0acc5d8fbcb86fd6e731b24c397bdb36c396e786
oberstrike <markus.juergens@gmx.de>
2019-01-29 10:01:15 +0100

    Update Configuration.cs

--- SQLiteApplication/UserData/Configuration.cs
+++ SQLiteApplication/UserData/Configuration.cs
@@ -10,9 +10,13 @@ namespace SQLiteApplication.UserData
     public class Configuration
     {   
         public string[] FarmingVillages { get; set; }
+        public string[] BuildOrder {get; set;}
         public User User { get; set; }
         public bool Build { get; set; }
         public bool Farm { get; set; }
+        public bool Trade {get; set;}
+        public int TimeToWait{get; set;}
+        
         public Configuration()
         {
 
